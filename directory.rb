d018d21120dc5c369ac31c90b81fa594084bb09e
Ben Partington <bdpartington@gmail.com>
2022-01-11 14:57:03 +0000

    added user input to add names to hash

--- directory.rb
+++ directory.rb
@@ -1,33 +1,38 @@
-# let's put all students into an array
-students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
-]
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print_names(names)
-  names.each do |student|
-    puts student
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 
-# nothing happens until we call the methods
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    #create an empty array
+    students = []
+    #get the first name
+    name = gets.chomp
+    #while the name is not empty, repeat this code
+    while !name.empty? do
+        #add the student hash to the array
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        #get another name from the user
+        name = gets.chomp
+    end
+    #return the array of students
+    students
+end
+
+students = input_students
 print_header
-print_names(students)
+print(students)
 print_footer(students)
